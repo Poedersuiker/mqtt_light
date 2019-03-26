9a0dd62bb6a7c462acf47f03bf1e9f3e07b44545
Peter Hendriks <peter@the-underground.nl>
2019-03-26 22:03:43 +0100

    Refactoring PEP8 Added functions for MQTTLight

---
+++ LightsGroundFloor.py
@@ -0,0 +1,14 @@
+# Python imports
+from time import sleep
+# 3rd party imports
+
+# module imports
+from MQTTLight import MQTTLight
+
+kitchen_light = MQTTLight("Kitchen", 1, 7)
+
+i = 0
+while i < 10:
+    kitchen_light.switch_light()
+    sleep(5)
+    i += 1
