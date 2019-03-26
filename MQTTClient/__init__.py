25d2f48808c64c1baac38c25b5695929d5c347da
Peter Hendriks <peter@the-underground.nl>
2019-03-26 21:47:10 +0100

    Refactoring PEP8 Added functions for MQTTLight

--- MQTTClient/__init__.py
+++ MQTTClient/__init__.py
@@ -4,7 +4,7 @@ import threading
 
 class MQTTClient(threading.Thread):
     def __init__(self, name, host="localhost", port=1883):
-        threading.Thread.__init__(self, target=self.run_forever)
+        threading.Thread.__init__(self, target=self.run)
         self.mqtt_client = mqtt.Client()
         self.host = host
         self.port = port
