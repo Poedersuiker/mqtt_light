620b7260ec0bd60071d771ca3c691b6b6ccb02e0
Peter Hendriks <peter@the-underground.nl>
2019-05-03 15:30:06 +0200

    Quick and Dirty light implementation

--- QDlights.py
+++ QDlights.py
@@ -14,23 +14,35 @@ pin2 = 15
 relay1 = 29
 relay2 = 31
 
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(pin2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(relay1, GPIO.OUT)
 GPIO.setup(relay2, GPIO.OUT)
 
+pin1_state = GPIO.input(pin1)
+pin2_state = GPIO.input(pin2)
+
 
 def switch_light(nr):
     GPIO.output(nr, not GPIO.input(nr))
 
 
-def my_callback(channel):
+def my_callback1(channel):
     print("falling edge detected on {0}".format(channel))
+    if pin1_state != GPIO.input(pin1):
+        print("State changed for {0}".format(channel))
 
 
-GPIO.add_event_detect(pin1, GPIO.FALLING, callback=my_callback, bouncetime=300)
-GPIO.add_event_detect(pin2, GPIO.FALLING, callback=my_callback, bouncetime=300)
+def my_callback2(channel):
+    print("falling edge detected on {0}".format(channel))
+    if pin2_state != GPIO.input(pin2):
+        print("State changed for {0}".format(channel))
+
+
+GPIO.add_event_detect(pin1, GPIO.FALLING, callback=my_callback1, bouncetime=300)
+GPIO.add_event_detect(pin2, GPIO.FALLING, callback=my_callback2, bouncetime=300)
 
 while True:
     try:
