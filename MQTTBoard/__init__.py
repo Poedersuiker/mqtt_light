53a95a27bc98860772ce56a8744e7d7066c7dd6b
Peter Hendriks <peter@the-underground.nl>
2020-05-04 16:48:50 +0200

    Quick and Dirty light implementation

--- MQTTBoard/__init__.py
+++ MQTTBoard/__init__.py
@@ -56,7 +56,7 @@ class MQTTBoard:
         RELAY = [False, 29, 31, 33, 36, 35, 38, 40, 37]
         RELAY_NAME = [False, 'UtilitiesRoom', 'ClosetMasterBedroom', 'light3', 'light4', 'light5', 'light6', 'light7', 'light8']
         SENSOR = [False, 3, 5, 7, 11, 13, 15, 19, 21]  # Change to sensor pins
-        SENSOR_NAME = [False, 'sensor1', 'sensor2', 'sensor3', 'sensor4', 'sensor5', 'sensor6', 'sensor7', 'sensor8']
+        SENSOR_NAME = [False, 'SwitchMasterBedroom1', 'SwitchMasterBedroom2', 'sensor3', 'sensor4', 'sensor5', 'sensor6', 'sensor7', 'sensor8']
         SENSOR_STATE = [False, None, None, None, None, None, None, None, None]
     else:
         print("Hostname not found")
